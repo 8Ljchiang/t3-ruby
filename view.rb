69538675bf578453a39a204b2277270756cce1ad
Jonathan C <jchiang@8thlight.com>
2018-10-24 15:27:37 -0500

    added View methods

--- view.rb
+++ view.rb
@@ -1,15 +1,34 @@
+require_relative './constants.rb'
+require_relative './game_renderer.rb'
+require_relative './board_rendere.rb'
+
 class View
     def initialize(args={})
-        @game_renderer = args[:game_renderer]
-        @board_renderer = args[:board_renderer]
+        @game_renderer = args[:game_renderer] || GameRenderer.new
+        @board_renderer = args[:board_renderer] || BoardRenderer.new
     end
 
     def show(text)
+        puts text
     end
 
     def render_game(game)
+        case game.state
+        when GAME_STATE_NEW
+            show(@game_renderer.welcome(game))
+        when GAME_STATE_STATED
+            _render_board(game.board)
+            show(@game_renderer.render(game))
+        when GAME_STATE_END
+            show(@game_renderer.end(game))
+        when GAME_STATE_CLOSED
+            show("The has been quit.")
+        else
+            show("Error: Unknown Game state.")
+        end
     end
 
     def _render_board(board)
+        show(@board_renderer.render(board))
     end
 end
