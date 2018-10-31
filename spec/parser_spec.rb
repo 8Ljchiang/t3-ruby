653e4a77711734ecbd3247e81c91b2ecdb2975f5
Jonathan C <jchiang@8thlight.com>
2018-10-31 14:44:07 -0500

    added tests for valid and invalid input

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -18,16 +18,47 @@ parserHandlers = {
     options: options
 }
 
+
 def create_parser(handlers)
     parser = Parser.new({ handlers: handlers })
     return parser
 end
 
 RSpec.describe(Parser) do
+
+    def create_parser_handlers_mock() {
+        mockHandlers = {}
+        allow(mockHandlers).to_receive(:error)
+        allow(mockHandlers).to_receive(:default)
+        allow(mockHandlers).to_receive(:options).and_return(["valid_input"])
+        return mockHandlers
+    }
+
     context "When creating a Parser class" do 
         it "should return an instantiated Parser object", positive: true do
             parser = create_parser(parserHandlers)
             expect(parser).to_not be(nil)
+        end
+    end
+
+    context "#parse" do
+        before(:each) do
+            mockHandlers = create_moack_parser_handlers()
+            @parser = create_parser(mockHandlers)
+        end
+
+        it "should take valid input and call the default handler", positive: true do
+            input = "valid_input"
+            @parser.parse(input)
+            expect(@parser.handlers).to have_received(:options)
+            expect(@parser.handlers).to have_received(:default)
+        end
+        
+        it "should take invalid input and call the error handler", positiive: true do
+            input = "invalid_input"
+            @parser.parse(input)
+            expect(@parser.handlers).to have_recieved(:options)
+            expect(@parser.handlers).to have_received(:error)
         end
     end
 end
