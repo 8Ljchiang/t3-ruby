4664b3fa1a43d469185669f171964a2b41747e75
Jonathan C <jchiang@8thlight.com>
2018-11-14 16:48:52 -0600

    added win condition check

--- lib/handler_started.rb
+++ lib/handler_started.rb
@@ -1,4 +1,5 @@
 require_relative './handler.rb'
+require_relative './pattern_checker.rb'
 
 class StartedHandler < Handler
     def handle(input, game)
@@ -22,7 +23,24 @@ class StartedHandler < Handler
     end
 
     def default(input, game)
+
+        default_win_patterns = [
+            "1,5,9",
+            "3,5,7",
+            "1,2,3",
+            "4,5,6",
+            "7,8,9",
+            "1,4,7",
+            "2,5,8",
+            "3,6,9"
+        ]
+        
+        pattern_checker = PatternChecker.new({ patterns: default_win_patterns })
         position = input.to_i
+        current_mark = game.current_player().mark
         game.play_a_position(position)
+        if pattern_checker.contains_matching_pattern(game.board.get_marker_positions(current_mark))
+            game.set_state("end")
+        end
     end
 end
