5e7d6e14e4643c48e2056d341df0e1e96ff50e94
Jonathan C <jchiang@8thlight.com>
2019-01-18 09:34:49 -0600

    create process_game_iteration function

--- lib/t3_game_utils.rb
+++ lib/t3_game_utils.rb
@@ -1,17 +1,23 @@
 require_relative './constants.rb'
 require_relative './t3_move_gen_utils.rb'
 
-current_game_status = determine_game_status(game, pattern_checker)
-game.set_game_status(current_game_status)
-case current_game_status
-when GAME_STATE_STARTED
-
-when GAME_STATE_DRAW
-
-when GAME_STATE_WINNER
-  set_game_winning_info(game, pattern_checker)
+def process_game_iteration(process_args)
+  input = process_args.fetch(:input)
+  game = process_args.fetch(:game)
+  pattern_checker = process_args.fetch(:pattern_checker)
+  with_ai = process_args.fetch(:with_ai)
+  current_game_status = determine_game_status(game, pattern_checker)
+  game.set_game_status(current_game_status)
+  case current_game_status
+  when GAME_STATE_STARTED
+    play_round(game, input, with_ai)
+  when GAME_STATE_DRAW
+  when GAME_STATE_WINNER
+    set_game_winning_info(game, pattern_checker)
+  end
 end
-def play_round(game, position_to_play, pattern_checker, with_ai = true)
+
+def play_round(game, position_to_play, with_ai = true)
     if with_ai
       move(game, position_to_play)
       ai_move = generate_random_move(game.board)
