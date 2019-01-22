a27f9644faa3db45e22e369b5a2f98f856085c9f
Jonathan C <jchiang@8thlight.com>
2019-01-22 13:13:23 -0600

    updated project structure

--- lib/playground_main.rb
+++ lib/playground_main.rb
@@ -1,53 +1,53 @@
-require_relative './class/board_renderer.rb'
-require_relative './constants/constants.rb'
-require_relative './class/game.rb'
-require_relative './class/pattern_checker.rb'
-require_relative './class/player.rb'
-require_relative './utils/t3_game_utils.rb'
-require_relative './utils/t3_pattern_utils.rb'
-require_relative './utils/t3_input_utils.rb'
+# require_relative './class/board_renderer.rb'
+# require_relative './constants/constants.rb'
+# require_relative './class/game.rb'
+# require_relative './class/pattern_checker.rb'
+# require_relative './class/player.rb'
+# require_relative './utils/t3_game_utils.rb'
+# require_relative './utils/t3_pattern_utils.rb'
+# require_relative './utils/t3_input_utils.rb'
 
-# Initialize players
-player1 = Player.new(DEFAULT_P1, MARKER_1)
-player2 = Player.new(DEFAULT_P2, MARKER_2)
+# # Initialize players
+# player1 = Player.new(DEFAULT_P1, MARKER_1)
+# player2 = Player.new(DEFAULT_P2, MARKER_2)
 
-game_args = { players: [player1, player2] }
+# game_args = { players: [player1, player2] }
 
-# Initialize Game
-tictactoe = Game.new(game_args)
-tictactoe.set_game_status('started')
+# # Initialize Game
+# tictactoe = Game.new(game_args)
+# tictactoe.set_game_status('started')
 
-# Initialize pattern checking
-winning_patterns = T3Engine::PatternGenerationUtils.generate_winning_patterns(tictactoe.board)
+# # Initialize pattern checking
+# winning_patterns = T3Engine::PatternGenerationUtils.generate_winning_patterns(tictactoe.board)
 
-# puts winning_patterns
-tictactoe_pattern_checker = PatternChecker.new(patterns: winning_patterns)
+# # puts winning_patterns
+# tictactoe_pattern_checker = PatternChecker.new(patterns: winning_patterns)
 
-# Play rounds of the game
-total_positions = tictactoe.board.width * tictactoe.board.height
-(1..total_positions).each do |position|
-  puts " >> playing #{position}"
-  move_position = T3Engine::InputUtils.parse_move_input(position)
-  is_valid_move = T3Engine::GameUtils.is_move_valid(tictactoe, move_position)
+# # Play rounds of the game
+# total_positions = tictactoe.board.width * tictactoe.board.height
+# (1..total_positions).each do |position|
+#   puts " >> playing #{position}"
+#   move_position = T3Engine::InputUtils.parse_move_input(position)
+#   is_valid_move = T3Engine::GameUtils.is_move_valid(tictactoe, move_position)
 
-  if is_valid_move
-    process_args = {
-      position: move_position,
-      game: tictactoe,
-      pattern_checker: tictactoe_pattern_checker,
-      with_ai: true
-    }
-    T3Engine::GameUtils.process_iteration(process_args)
-  end
+#   if is_valid_move
+#     process_args = {
+#       position: move_position,
+#       game: tictactoe,
+#       pattern_checker: tictactoe_pattern_checker,
+#       with_ai: true
+#     }
+#     T3Engine::GameUtils.process_iteration(process_args)
+#   end
 
-  if tictactoe.game_status != GAME_STATE_STARTED
-    break
-  end
-end
+#   if tictactoe.game_status != GAME_STATE_STARTED
+#     break
+#   end
+# end
 
-# Show end game information
-br = BoardRenderer.new
-puts ' >> Each loop finished'
-puts "Final game state: #{tictactoe.game_status}"
-puts 'Final board: '
-puts br.render(tictactoe.board)
+# # Show end game information
+# br = BoardRenderer.new
+# puts ' >> Each loop finished'
+# puts "Final game state: #{tictactoe.game_status}"
+# puts 'Final board: '
+# puts br.render(tictactoe.board)
---
+++ lib/t3.rb
@@ -0,0 +1,44 @@
+require 'class/app'
+require 'class/handler_end.rb'
+require 'class/handler_new.rb'
+require 'class/handler_started.rb'
+require 'utils/t3_pattern_utils.rb'
+
+module Main
+  def self.start
+    # Initialize players
+    player1 = Player.new(DEFAULT_P1, MARKER_1)
+    player2 = Player.new(DEFAULT_P2, MARKER_2)
+
+    game_args = { players: [player1, player2] }
+
+    # Initialize Game
+    tictactoe = Game.new(game_args)
+    tictactoe.set_game_status('started')
+
+    # Initialize pattern checking
+    winning_patterns = T3Engine::PatternGenerationUtils.generate_winning_patterns(tictactoe.board)
+
+    # puts winning_patterns
+    tictactoe_pattern_checker = PatternChecker.new(patterns: winning_patterns)
+
+    new_handler = NewHandler.new
+    started_handler = StartedHandler.new(pattern_checker: tictactoe_pattern_checker)
+    end_handler = EndHandler.new
+
+    handler_table = {
+      'new': new_handler,
+      'started': started_handler,
+      'end': end_handler
+    }
+
+    app_args = {
+      game: tictactoe,
+      handler_table: handler_table
+    }
+
+    t3 = App.new(app_args)
+
+    t3.run
+  end
+end
